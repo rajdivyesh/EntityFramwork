ada6ccd2572e18d0c78a6f30d012c483b8473e4b
Divyesh Shailendra Raj <divyesh_shailendra.raj@lcieducation.net>
2023-03-17 15:48:54 -0400

    Perform Update in the Table

---
+++ Update/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Update
+{
+    internal class Program
+    {
+        static EMPEntities db = new EMPEntities();
+        static void Main(string[] args)
+        {
+            var result = db.COMPANY.Where(s => s.ID == 1).SingleOrDefault();
+            if (result != null)
+            {
+                result.SALARY = (decimal)25000.00;
+            }
+
+            var result2 = db.COMPANY.Where(s => s.ID == 2).SingleOrDefault();
+            if (result2 != null) 
+            {
+                db.COMPANY.Remove(result2);
+            }
+            else
+            {
+                Console.WriteLine("Employee not found for removal");
+            }
+
+            try
+            {
+                db.SaveChanges();
+                Console.WriteLine("Data successfully Updated");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Data not Updated");
+            }
+
+            foreach (var c in db.COMPANY)
+            {
+                Console.WriteLine("Id = {0}; Name = {1}", c.ID, c.NAME);
+                Console.WriteLine("Age = {0}", c.AGE);
+                Console.WriteLine("Address = {0}", c.ADDRESS);
+                Console.WriteLine("Salary = {0:F2}", c.SALARY);
+                Console.WriteLine("Salary = {0}", c.SALARY.Value.ToString("F2"));
+                Console.WriteLine("Salary = {0}", c.SALARY.Value.ToString("N2"));
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
