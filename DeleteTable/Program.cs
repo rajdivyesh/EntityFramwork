c954c4c7c1f83ff47336a9302106192e219a0709
Divyesh Shailendra Raj <divyesh_shailendra.raj@lcieducation.net>
2023-03-22 16:46:02 -0400

    Delete Table Syntax

---
+++ DeleteTable/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeleteTable
+{
+    internal class Program
+    {
+        static EMPEntities db = new EMPEntities();
+        static void Main(string[] args)
+        {
+            try
+            {
+                db.Database.ExecuteSqlCommand("DROP TABLE COMPANY", new object[0]);
+                Console.WriteLine("Table Successfully deleted!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Tables not deleted");
+            }
+            Console.ReadKey();    
+        }
+    }
+}
